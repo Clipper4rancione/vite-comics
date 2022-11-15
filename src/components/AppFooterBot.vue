ff5c475524df15958226d983f821b36d81602c16
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-15 21:40:26 +0100

    footerbot completed

--- src/components/AppFooterBot.vue
+++ src/components/AppFooterBot.vue
@@ -9,8 +9,13 @@ export default {
     <div class="container">
       <div class="wrapper">
         <div class="sign-up-btn">sign-up now!</div>
-        <div class="social section">
+        <div class="social-section">
           <span>follow us</span>
+          <img src="../assets/img/footer-facebook.png" alt="" />
+          <img src="../assets/img/footer-twitter.png" alt="" />
+          <img src="../assets/img/footer-youtube.png" alt="" />
+          <img src="../assets/img/footer-pinterest.png" alt="" />
+          <img src="../assets/img/footer-periscope.png" alt="" />
         </div>
       </div>
     </div>
@@ -26,12 +31,26 @@ export default {
     color: white;
     text-transform: uppercase;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 
     .sign-up-btn {
+      cursor: pointer;
       font-size: 1.4rem;
       border: 2px solid #3b7ef0;
       padding: 10px 25px;
+    }
+    .social-section {
+      display: flex;
+      align-items: center;
+      span {
+        font-size: 1.5rem;
+        color: #3b7ef0;
+      }
+      img {
+        vertical-align: middle;
+        margin-left: 15px;
+      }
     }
   }
 }
