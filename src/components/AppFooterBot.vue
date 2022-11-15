341174be4dd152885819232d2ed7db4779451c2a
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-15 20:20:51 +0100

    created a bottom footer

---
+++ src/components/AppFooterBot.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "AppFooterBot",
+};
+</script>
+
+<template>
+  <section class="bot-footer">
+    <div class="container">
+      <div class="wrapper">
+        <div class="sign-up-btn">sign-up now!</div>
+        <div class="social section">
+          <span>follow us</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+.bot-footer {
+  padding: 15px;
+  background-color: #333333;
+  .wrapper {
+    color: white;
+    text-transform: uppercase;
+    display: flex;
+    justify-content: space-between;
+
+    .sign-up-btn {
+      font-size: 1.4rem;
+      border: 2px solid #3b7ef0;
+      padding: 10px 25px;
+    }
+  }
+}
+</style>
