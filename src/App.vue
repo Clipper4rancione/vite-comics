1d388e9234a5c343605fce9b109da14034d38bf3
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-15 17:11:54 +0100

    imported components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+import AppMain from "./components/AppMain.vue";
+
 export default {
   name: "Vue",
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
 };
 </script>
 
 <template>
-  <h1>PROVA</h1>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 
