a2ff72ea44c91e83aac25000363e0eec3964c8bb
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-11-16 16:36:38 +0100

    [add] jumbotron

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "AppJumbo",
+};
+</script>
+
+<template>
+  <div class="j-container"></div>
+</template>
+
+
+<style lang="scss" scoped>
+.j-container {
+  width: 100%;
+  height: 450px;
+
+  background-image: url("../assets/img/jumbotron.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
